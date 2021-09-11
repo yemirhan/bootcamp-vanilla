f2b083a885cc8d7dae8174d50deb65df62663820
Yusuf Emirhan <syusufemirhan@gmail.com>
2021-09-11 11:41:50 +0300

    added styling for table

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { TableBody } from './TableBody'
 import { TableHeader } from './TableHeader'
-
+import "../styles/tablestyle.css"
 export const Table = () => {
     return (
         <table>
