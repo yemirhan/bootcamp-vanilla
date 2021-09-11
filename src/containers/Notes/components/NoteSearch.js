71ae16957f9b706efb83c2ffd5eeafb772f5934c
Yusuf Emirhan <syusufemirhan@gmail.com>
2021-09-11 16:21:26 +0300

    Search bar styling

--- src/containers/Notes/components/NoteSearch.js
+++ src/containers/Notes/components/NoteSearch.js
@@ -1,7 +1,10 @@
 import React from 'react'
-
+import searchIcon from "../assets/Search_Icon.svg"
 export const NoteSearch = ({ search = "", setSearch = () => { } }) => {
     return (
-        <input type="search" className="w-full border-2 border-gray-200 rounded-lg" value={search} onChange={({ target: { value } }) => setSearch(value)} />
+        <div className="w-full px-2 py-2 bg-white inline-flex">
+            <input type="search" className="w-full border-2 border-gray-200 rounded-lg p-2 mr-1" value={search} onChange={({ target: { value } }) => setSearch(value)} />
+            <img src={searchIcon} alt="search" className="w-7" />
+        </div>
     )
 }
--- src/containers/Notes/components/Tasks.js
+++ src/containers/Notes/components/Tasks.js
@@ -8,12 +8,19 @@ export const Tasks = ({ setSelectedNote, notes, selectedNote }) => {
 
     return (
         <div className="w-96 bg-white h-screen flex flex-col border-r border-gray-100">
-            <div className="w-full px-2 py-1 bg-white">
-                <NoteSearch search={search} setSearch={setSearch} />
-
-            </div>
-
-            {notes.filter(note => note.noteTitle.includes(search)).map(note => <Note key={note.createdAt} selectedNote={selectedNote} setSelectedNote={setSelectedNote} note={note} />)}
+            <NoteSearch search={search} setSearch={setSearch} />
+            {notes
+                .filter(note => note.noteTitle.includes(search))
+                .map(note => (
+                    <Note
+                        key={note.createdAt}
+                        selectedNote={selectedNote}
+                        setSelectedNote={setSelectedNote}
+                        note={note}
+                    />
+                )
+                )
+            }
         </div>
     )
 }
