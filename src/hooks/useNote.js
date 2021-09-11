3d5e55c8e821f77a4c2da0f923b5ff99dbaa2416
Yusuf Emirhan <syusufemirhan@gmail.com>
2021-09-11 16:17:07 +0300

    useNote for offline use

--- src/hooks/useNote.js
+++ src/hooks/useNote.js
@@ -4,33 +4,33 @@ export const useNote = () => {
     const [notes, setNotes] = useState([])
     const [selectedNote, setSelectedNote] = useState(null)
     useEffect(() => {
-        setNotes([{
-            noteTitle: "Test note",
-            note: "Bu bir not",
-            createdAt: 194209
-        },
-        {
-            noteTitle: "Test note2",
-            note: "Bu bir not iki",
-            createdAt: 8723945234
-        }])
+        setNotes(JSON.parse(localStorage.getItem("notes") || "[]"))
         return () => {
             setNotes()
         }
     }, [])
+    const saveNoteToStorage = (n) => {
+        localStorage.setItem("notes", JSON.stringify(n))
+    }
     const updateNote = (createdAt, note, title) => {
         const _notes = [...notes]
         _notes.splice(notes.findIndex(e => e.createdAt === createdAt), 1, { createdAt, note, noteTitle: title })
-        setNotes([..._notes])
+        setNotes(() => { saveNoteToStorage([..._notes]); return [..._notes] })
     }
     const createNote = () => {
         const cAt = Date.now()
-        setNotes(noteList => ([...noteList, { noteTitle: "", note: "", createdAt: cAt }]))
-        setSelectedNote(cAt)
+        setNotes(noteList => {
+            saveNoteToStorage([...noteList, { noteTitle: "", note: "", createdAt: cAt }]);
+            return [...noteList, { noteTitle: "", note: "", createdAt: cAt }]
+        })
+        setSelectedNote({ noteTitle: "", note: "", createdAt: cAt })
     }
     const deleteNote = (createdAt) => {
         setSelectedNote(null)
-        setNotes(n => [...n.filter(d => d.createdAt !== createdAt)])
+        setNotes(n => {
+            saveNoteToStorage([...n.filter(d => d.createdAt !== createdAt)]);
+            return [...n.filter(d => d.createdAt !== createdAt)]
+        })
 
     }
     return {
