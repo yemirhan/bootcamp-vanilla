accd5856bd235612eb50e313c94ef653947cdfd8
Yusuf Emirhan <syusufemirhan@gmail.com>
2021-09-11 12:11:01 +0300

    tableheader comment

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -1,9 +1,9 @@
 import React from 'react'
 
 export const TableHeader = () => {
-    return (
+    return ( //I Could've also used Object.keys to get the keys of every object of the data, but because the example has very little fiels, i chose not to
         <thead>
-            <tr>
+            <tr> 
                 <th>Döviz Cinsi</th>
                 <th>Alış(TL)</th>
                 <th>Satış(TL)</th>
