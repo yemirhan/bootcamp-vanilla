2e9f0acec3bd4cea5cd1247745a472c7ac1ddec3
Yusuf Emirhan <syusufemirhan@gmail.com>
2021-09-12 10:18:27 +0300

    animate fix

--- src/containers/Notes/components/Note.js
+++ src/containers/Notes/components/Note.js
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion'
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useSelector } from 'react-redux'
@@ -9,19 +10,20 @@ import { NoteArea } from './NoteArea'
 import { NoteFooter } from './NoteFooter'
 import { TitleBar } from './TitleBar'
 
-export const Note = ({ match: { params: { id }, ...match }, ...rest }) => {
+export const Note = ({ match: { params: { id } } }) => {
     const [note, setNote] = useState({})
     const $notes = useSelector(state => state.$notes)
     const history = useHistory()
     const dispatch = useDispatch()
     const $n = useNote()
     useEffect(() => {
-        if (match.path === "/create") { setNote({ createdAt: null, noteTitle: "", note: "" }); dispatch($edit()) }
-        else setNote(($notes.notes || []).find(d => d.createdAt === parseInt(id)))
+        // if (m.path === "/create") { setNote({ createdAt: null, noteTitle: "", note: "" }); dispatch($edit()) }
+        // else 
+        setNote(($notes.notes || []).find(d => d.createdAt === parseInt(id)))
         return () => {
             setNote({})
         }
-    }, [id, $notes.notes, match.path])
+    }, [])
 
     if (!note) return <div className="w-full h-screen bg-white flex flex-col items-center justify-center space-y-2">
         <p className="font-semibold text-xl">Bu not mevcut değil.</p>
@@ -29,10 +31,10 @@ export const Note = ({ match: { params: { id }, ...match }, ...rest }) => {
     </div>
 
     return (
-        <div className="w-full h-screen bg-gray-700 flex flex-col">
+        <motion.div exit={{ x: "-100vw", opacity: 0 }} className="w-full h-screen bg-gray-700 flex flex-col">
             <TitleBar setSelectedNote={() => history.push("/")} note={note} setNote={setNote} />
             <NoteArea note={note} setNote={setNote} />
             <NoteFooter note={note} deleteNote={$n.deleteNote} updateNote={$n.updateNote} />
-        </div>
+        </motion.div>
     )
 }
--- src/containers/Notes/modules/ViewPanel.js
+++ src/containers/Notes/modules/ViewPanel.js
@@ -1,16 +1,21 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import React, { useEffect, useState } from 'react'
 import {
     Switch,
-    Route
+    Route,
+    useHistory
 } from "react-router-dom";
 import { Note } from '../components/Note';
 
 export const ViewPanel = ({ selectedNote, notes = [], setSelectedNote = () => { }, updateNote = () => { }, deleteNote = () => { } }) => {
+    const history = useHistory()
     return <div className="w-full h-screen bg-gray-700 flex flex-col">
-        <Switch>
-            <Route path={"/"} exact render={(props) => <div className="w-full h-screen bg-white flex items-center justify-center">Sol taraftan bir not seçiniz.</div>} />
-            <Route path={"/note/:id"} render={(props) => <Note {...props} />} />
-            <Route path={"/create"} render={(props) => <Note {...props} />} />
-        </Switch>
+        <AnimatePresence exitBeforeEnter>
+            <Switch location={history.location} key={history.location.pathname}>
+                <Route path={"/"} exact render={(props) => <motion.div exit={{ x: "-100vw", opacity: 0 }} key={Date.now()} className="w-full h-screen bg-white flex items-center justify-center">Sol taraftan bir not seçiniz.</motion.div>} />
+                <Route path={"/note/:id"} render={(props) => <Note key={Date.now()} {...props} />} />
+                <Route path={"/create"} render={(props) => <Note key={Date.now()} {...props} />} />
+            </Switch>
+        </AnimatePresence>
     </div>
 }
